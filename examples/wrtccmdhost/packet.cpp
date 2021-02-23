07199f08be27eb92df0ed733587f0bcf7f39e67a
yujia <yujia@qiniu.com>
2021-02-23 10:48:49 +0800

    fix_url_stream_scale_diviation

--- examples/wrtccmdhost/packet.cpp
+++ examples/wrtccmdhost/packet.cpp
@@ -22,7 +22,7 @@ MediaPacket::MediaPacket(IN const AVStream& _avStream, IN const AVPacket* _pAvPa
 }
 
 MediaPacket::MediaPacket(
-        IN StreamType stream, IN CodecType codec, 
+        IN StreamType stream, IN CodecType codec,
         const std::vector<uint8_t>& extradata, const std::vector<uint8_t>& data
 ) : stream_(stream), codec_(codec), extradata(extradata), data(data)
 {
@@ -237,14 +237,15 @@ MediaFrame::MediaFrame(IN int _nWidth, IN int _nHeight, IN AVPixelFormat _format
         pAvFrame_->width = _nWidth;
         pAvFrame_->height = _nHeight;
         av_frame_get_buffer(pAvFrame_, 32);
+        const AVPixFmtDescriptor *desc = av_pix_fmt_desc_get(_format);
 
         // fill up the picture with pure color
-        if (_nColor >= 0) {
+        if (_nColor >= 0 && desc) {
                 uint8_t nY, nU, nV;
                 color::RgbToYuv(_nColor, nY, nU, nV);
                 memset(pAvFrame_->data[0], nY, pAvFrame_->linesize[0] * _nHeight);
-                memset(pAvFrame_->data[1], nU, pAvFrame_->linesize[1] * (_nHeight / 2));
-                memset(pAvFrame_->data[2], nV, pAvFrame_->linesize[2] * (_nHeight / 2));
+                memset(pAvFrame_->data[1], nU, pAvFrame_->linesize[1] * FF_CEIL_RSHIFT(_nHeight, desc->log2_chroma_h));
+                memset(pAvFrame_->data[2], nV, pAvFrame_->linesize[2] * FF_CEIL_RSHIFT(_nHeight, desc->log2_chroma_h));
                 if (pAvFrame_->data[3]) {
                         memset(pAvFrame_->data[3], 0xff, pAvFrame_->linesize[3] * _nHeight);
                 }
